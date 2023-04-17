e6ef70307716c2f2d5fa04de3e68ceb91292ca23
vipul440 <82093511+vipul440@users.noreply.github.com>
2023-04-17 11:24:21 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -2,8 +2,32 @@ import React from 'react'
 import '../styles/App.css';
 
 class App extends React.Component {
-  
+  constructor(props) {
+    super(props);
+    this.state = {
+      name: "John Doe",
+      enrollmentNo: "12345678",
+      age: 34
+    };
+    this.handleButtonClick = this.handleButtonClick.bind(this);
 }
+  handleButtonClick() {
+    this.setState((prevState) => ({
+      age: prevState.age + 1
+    }));
+  }
 
-
+  render() {
+    const { name, enrollmentNo, age } = this.state;
+    return (
+      <div>
+        <h1>Hello, my name is {name}</h1>
+        <p>
+          I am {age} years old and my enrollment no is {enrollmentNo}
+        </p>
+        <button onClick={this.handleButtonClick}>Increase Age</button>
+      </div>
+    );
+  }
+}
 export default App;
